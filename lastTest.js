041683712b6e9f6f422bf086f416f6fc1ed044bb
BÙI VIỆT DŨNG <117715149+dungkkk7@users.noreply.github.com>
2025-03-24 14:39:15 +0700

    message

--- lastTest.js
+++ lastTest.js
@@ -66,11 +66,12 @@ class BlogManager {
             container.appendChild(postElement);
             console.log(`Đã thêm bài viết ${index + 1} vào container`);
             // index = 5 xóa 
+            if (index === 3) {
+                return;
+            }
            
         });
-        if (index === 3) {
-            return;
-        }
+       
         
         console.log('Hoàn thành việc tải các bài viết mới');
     }
